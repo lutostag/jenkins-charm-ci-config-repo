b2ac56e208465b67aff50b22b726a978ad4c25ef
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-13 16:54:58 -0500

    use correct bundle git repo

--- jenkins/jobs/deploy.sh
+++ jenkins/jobs/deploy.sh
@@ -4,11 +4,9 @@ rm -fr *
 juju show-controller jenkins-clean-test >/dev/null ||
 echo -e 'jenkins-clean-test\npass\npass' | juju register $(vault-client read secret/jenkins-clean-test | awk '/key/{print $2}')
 
-git clone https://github.com/lutostag/jenkins-charm-ci-config-repo.git -b self-ci
-cd jenkins-charm-ci-config-repo
-juju deploy bundle.yaml
+git clone https://github.com/lutostag/jenkins-bundle-example.git -b self-ci
+juju deploy ./jenkins-bundle-example/bundle.yaml
 juju wait
-cd ..
 
 git clone https://github.com/lutostag/layer-jenkins.git -b built
 juju upgrade-charm -p layer-jenkins jenkins
