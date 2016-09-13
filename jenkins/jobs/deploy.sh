ddd2029259e825405f4ce6a9a254765521d14ebb
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-13 16:28:08 -0500

    use only key data from secret

--- jenkins/jobs/deploy.sh
+++ jenkins/jobs/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-juju show-model jenkins-clean-test || juju register $(vault-client read secret/jenkins-clean-test) 
+juju show-model jenkins-clean-test || juju register $(vault-client read secret/jenkins-clean-test | awk '/key/{print $2}') 
 
 git clone https://github.com/lutostag/jenkins-charm-ci-config-repo.git -b self-ci
 juju deploy -m jenkins-clean-test jenkins-charm-ci-config-repo/bundle.yaml
