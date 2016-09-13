7266a1540aad3e9c432c47ae6145cd4d343aed4f
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-13 16:50:52 -0500

    use ./ for local bundle path

--- jenkins/jobs/deploy.sh
+++ jenkins/jobs/deploy.sh
@@ -5,7 +5,7 @@ juju show-controller jenkins-clean-test >/dev/null ||
 echo -e 'jenkins-clean-test\npass\npass' | juju register $(vault-client read secret/jenkins-clean-test | awk '/key/{print $2}')
 
 git clone https://github.com/lutostag/jenkins-charm-ci-config-repo.git -b self-ci
-juju deploy jenkins-charm-ci-config-repo/bundle.yaml
+juju deploy ./jenkins-charm-ci-config-repo/bundle.yaml
 juju wait
 
 git clone https://github.com/lutostag/layer-jenkins.git -b built
