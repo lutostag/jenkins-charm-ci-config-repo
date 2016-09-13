22307774f06b2447666ff84c8391061a2271ed7d
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-13 15:07:22 -0500

    use self-ci branches and check scm every minute

--- jenkins/jobs/deploy.sh
+++ jenkins/jobs/deploy.sh
@@ -5,7 +5,9 @@ juju show-model jenkins-clean-test || juju register $(vault-client read secret/j
 
 git clone https://github.com/lutostag/jenkins-charm-ci-config-repo.git -b self-ci
 juju deploy -m jenkins-clean-test jenkins-charm-ci-config-repo/bundle.yaml
+juju wait -e jenkins-clean-test
 
 
 git clone https://github.com/lutostag/layer-jenkins.git -b built
 juju upgrade-charm -m jenkins-clean-test -p layer-jenkins jenkins
+juju wait -e jenkins-clean-test
