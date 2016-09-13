7c45fae1510ba27b976167ec6f4587e439e9ea5a
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-13 16:53:09 -0500

    cd into jenkins bundle dir

--- jenkins/jobs/deploy.sh
+++ jenkins/jobs/deploy.sh
@@ -5,8 +5,10 @@ juju show-controller jenkins-clean-test >/dev/null ||
 echo -e 'jenkins-clean-test\npass\npass' | juju register $(vault-client read secret/jenkins-clean-test | awk '/key/{print $2}')
 
 git clone https://github.com/lutostag/jenkins-charm-ci-config-repo.git -b self-ci
-juju deploy ./jenkins-charm-ci-config-repo/bundle.yaml
+cd jenkins-charm-ci-config-repo
+juju deploy bundle.yaml
 juju wait
+cd ..
 
 git clone https://github.com/lutostag/layer-jenkins.git -b built
 juju upgrade-charm -p layer-jenkins jenkins
