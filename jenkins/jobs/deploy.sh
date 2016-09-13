9ca8d5f2a6bc2a79ff77887cc2aa55ab761e14ae
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-13 16:42:02 -0500

    do not specify juju_env

--- jenkins/jobs/deploy.sh
+++ jenkins/jobs/deploy.sh
@@ -3,10 +3,9 @@ juju show-model jenkins-clean-test ||
 echo -e 'jenkins-clean-test\npass\npass' | juju register $(vault-client read secret/jenkins-clean-test | awk '/key/{print $2}')
 
 git clone https://github.com/lutostag/jenkins-charm-ci-config-repo.git -b self-ci
-juju deploy -m jenkins-clean-test jenkins-charm-ci-config-repo/bundle.yaml
-juju wait -e jenkins-clean-test
-
+juju deploy jenkins-charm-ci-config-repo/bundle.yaml
+juju wait
 
 git clone https://github.com/lutostag/layer-jenkins.git -b built
-juju upgrade-charm -m jenkins-clean-test -p layer-jenkins jenkins
-juju wait -e jenkins-clean-test
+juju upgrade-charm -p layer-jenkins jenkins
+juju wait
