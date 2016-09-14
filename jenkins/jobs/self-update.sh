d6a352ba9e8afb56080c9fe4bfea19c6911887b0
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-14 00:55:42 -0500

    spin on waiting for secret

--- jenkins/jobs/self-update.sh
+++ jenkins/jobs/self-update.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -xe
 rm -fr *
 
+# make sure we have a juju controller setup (or spin on waiting for secret to register)
 juju show-controller jenkins-clean-test >/dev/null ||
-echo -e 'jenkins-clean-test\npass\npass' | juju register $(vault-client read secret/jenkins-clean-test | awk '/key/{print $2}')
+echo "Waiting for registration secret to be passed into vault" &&
+while true;
+    do vault-client read -field=register-key secret/charm/juju-model >/dev/null && break; sleep 20;
+done &&
+echo -e 'jenkins-clean-test\npass\npass' | juju register $(vault-client read -field=register-key secret/charm/juju-model)
 
 git clone https://github.com/lutostag/jenkins-bundle-example.git -b self-ci
 juju deploy ./jenkins-bundle-example/bundle.yaml
@@ -12,4 +17,4 @@ git clone https://github.com/lutostag/layer-jenkins.git -b built jenkins
 juju upgrade-charm --path=./jenkins jenkins
 juju wait
 
-juju status | grep error >/dev/null && return 1~
+juju status | grep error >/dev/null && return 1
